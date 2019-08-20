6ed295665e7d538e44c9d1bd79b12811b7754b19
Mirco Mannino <mircomannino1997@gmail.com>
2019-08-20 12:16:39 +0200

    Adding loan_administrator.h and fix bug

---
+++ loan_administrator.h
@@ -0,0 +1,56 @@
+#include "loan.h"
+#include "date.h"
+#include <map>
+#include <fstream>
+
+class LoanAdministrator {
+private:
+    std::map<std::string, Loan> loans;
+    const std::string registerFile = "./register.txt";
+public:
+    /* Constructor */
+    LoanAdministrator() {}
+
+    /* Destructors */
+    ~LoanAdministrator() {
+        writeOnFile(registerFile);
+    }
+
+    /* Getter */
+    std::map<std::string, Loan> getLoans() const { return loans; }
+
+    /* Adding a loan */
+    void addLoan(const Loan& loan) {
+        loans[loan.getTitle()] = loan;
+    }
+    /* Adding from string */
+    void addLoanFromString(const std::string& loanString) {
+        Loan loan;
+        loan.readFromString(loanString);
+        this->addLoan(loan);
+    }
+
+    /* Write on file */
+    void writeOnFile(const std::string& file) {
+        std::ofstream outStream(file);
+        for(auto el : loans) {
+            outStream << el.second << std::endl;
+        }
+    }
+
+    /* Read from file */
+    void readFromFile(const std::string& file) {
+        std::ifstream inStream(file);
+        std::string line;
+        while(getline(inStream, line)) {
+            addLoanFromString(line);
+        }
+    }
+
+};
+/* Operator "<<" overloading */
+std::ostream& operator<<(std::ostream& out, const LoanAdministrator& loans) {
+    for(auto el : loans.getLoans()) {
+        out << el.second << std::endl;
+    }
+}
